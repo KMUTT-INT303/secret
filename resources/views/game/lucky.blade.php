90ac4a627176149c6e400cf481271a0e10d16697
tachittanachote <54389403+tachittanachote@users.noreply.github.com>
2020-01-26 23:28:43 +0700

    adjust lucky content

--- resources/views/game/lucky.blade.php
+++ resources/views/game/lucky.blade.php
@@ -1,12 +1,42 @@
 <lucky role="lucky" class="container">
     <div class="row">
-        <a href="daily-lucky?day=sunday"><div class="col-sm-1.25"><div class="sunday" data-hover="อาทิตย์">อา.</div></div></a>
-        <a href="daily-lucky?day=monday"><div class="col-sm-1.25 "><div class="monday" data-hover="จันทร์">จ.</div></div></a>
-            <a href="daily-lucky?day=teusday"><div class="col-sm-1.25"><div class="teusday" data-hover="อังคาร">อ.</div></div></a>
-                <a href="daily-lucky?day=wednesday"><div class="col-sm-1.25"><div class="wednesday" data-hover="พุธ">พ.</div></div></a>
-                    <a href="daily-lucky?day=thursday"><div class="col-sm-1.25"><div class="thursday" data-hover="พฤหัส">พฤ.</div></div></a>
-                        <a href="daily-lucky?day=friday"><div class="col-sm-1.25"><div class="friday" data-hover="ศุกร์">ศ.</div></div></a>
-                            <a href="daily-lucky?day=saturday"><div class="col-sm-1.25 "><div class="saturday" data-hover="เสาร์">ส.</div></div></a>
+        <div class="col-sm-12 d-flex justify-content-center">
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'sunday'])}}">
+                <div class="col-sm-1.25">
+                    <div class="sunday" data-hover="อาทิตย์">อา.</div>
+                </div>
+            </a>
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'monday'])}}">
+                <div class="col-sm-1.25">
+                    <div class="monday" data-hover="จันทร์">จ.</div>
+                </div>
+            </a>
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'tuesday'])}}">
+                <div class="col-sm-1.25">
+                    <div class="tuesday" data-hover="อังคาร">อ.</div> 
+                </div>
+            </a>
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'wednesday'])}}">
+                <div class="col-sm-1.25">
+                    <div class="wednesday" data-hover="พุธ">พ.</div>
+                </div>
+            </a>
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'thursday'])}}">
+                <div class="col-sm-1.25">
+                    <div class="thursday" data-hover="พฤหัส">พฤ.</div>
+                </div>
+            </a>
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'friday'])}}">
+                <div class="col-sm-1.25">
+                    <div class="friday" data-hover="ศุกร์">ศ.</div>
+                </div>
+            </a>
+            <a class="m-2" href="{{route('daily.lucky', ['day' => 'saturday'])}}">
+                <div class="col-sm-1.25 ">
+                    <div class="saturday" data-hover="เสาร์">ส.</div>
+                </div>
+            </a>
+        </div>
     </div>
 </lucky>
 
